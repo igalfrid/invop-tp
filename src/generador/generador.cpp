3967845c747c3a03ee9fa49f4014e040dc5e3c39
Marco Vanotti <mvanotti@dc.uba.ar>
2015-11-23 19:17:45 -0300

    Generador pone por lo menos 1 nodo en c/particion
    Ademas eliminamos dependencia en CPLEX que no se usaba.

--- src/generador/generador.cpp
+++ src/generador/generador.cpp
@@ -1,20 +1,22 @@
-#include <ilcplex/ilocplex.h>
-#include <ilcplex/cplex.h>
-ILOSTLBEGIN
 #include <string>
 #include <vector>
 #include <list>
+#include <set>
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-list< pair<int,int> > generarAristas(int cantidadDeNodos, double densidadDeAristas) {
-  list< pair<int,int> > aristas;
-  int cantidadDeAristas = 0;
-  for(int i = 1; i <= cantidadDeNodos; i++) {
-    for(int j = i + 1; j <= cantidadDeNodos; j++) {
-      double random = ((double) rand() / (RAND_MAX));
-      if(random <= densidadDeAristas) {
-        pair<int,int> arista = make_pair(i, j);
+list<pair<int, int>> generarAristas(int cantidadDeNodos,
+                                    double densidadDeAristas) {
+  list<pair<int, int>> aristas;
+  for (int i = 1; i <= cantidadDeNodos; i++) {
+    for (int j = i + 1; j <= cantidadDeNodos; j++) {
+      double random = ((double)rand() / (RAND_MAX));
+      if (random <= densidadDeAristas) {
+        pair<int, int> arista = make_pair(i, j);
         aristas.push_back(arista);
       }
     }
@@ -22,47 +24,70 @@ list< pair<int,int> > generarAristas(int cantidadDeNodos, double densidadDeArist
   return aristas;
 }
 
-void generarParticiones(ofstream &archivo, int cantidadDeNodos, int cantidadDeParticiones) {
+void generarParticiones(ofstream &archivo, int cantidadDeNodos,
+                        int cantidadDeParticiones) {
   // Creo las n particiones
-  list<int> particiones[cantidadDeNodos];
+  vector<set<int>> particiones(cantidadDeNodos);
 
-  // Recorro los nodos y coloco a cada nodo en la particion que le corresponde
-  for(int i=1; i<=cantidadDeNodos; i++) {
-    int particion = rand() % cantidadDeParticiones;
-    particiones[particion].push_back(i);
+  vector<int> nodosDisponibles;
+  // Agregamos todos los nodos.
+  for (int i = 1; i <= cantidadDeNodos; i++) {
+    nodosDisponibles.push_back(i);
+  }
+  random_shuffle(nodosDisponibles.begin(), nodosDisponibles.end());
+
+  random_device rd; // only used once to initialise (seed) engine
+  mt19937 rng(
+      rd()); // random-number engine used (Mersenne-Twister in this case)
+  uniform_int_distribution<int> uni(0, cantidadDeParticiones - 1);
+
+  for (int i = 0; i < cantidadDeParticiones; i++) {
+    particiones[i].insert(nodosDisponibles[i]);
   }
 
-  // Escrivo las particiones en el archivo
-  for(int i=0; i<cantidadDeParticiones; i++) {
+  for (int i = cantidadDeParticiones; i < cantidadDeNodos; i++) {
+    auto random_integer = uni(rng);
+    particiones[random_integer].insert(nodosDisponibles[i]);
+  }
+
+  // Escribo las particiones en el archivo
+  for (int i = 0; i < cantidadDeParticiones; i++) {
     archivo << "v " << particiones[i].size();
-    list<int>::iterator it;
-    for (it=particiones[i].begin(); it!=particiones[i].end(); ++it) {
-      archivo << " " << *it;
+    for (const auto &n : particiones[i]) {
+      archivo << " " << n;
     }
+
     archivo << endl;
   }
 }
 
-void escribirAristas(ofstream &archivo, list< pair<int,int> > aristas) {
-  list< pair<int,int> >::iterator it;
-  for (it=aristas.begin(); it!=aristas.end(); ++it) {
+void escribirAristas(ofstream &archivo, list<pair<int, int>> aristas) {
+  list<pair<int, int>>::iterator it;
+  for (it = aristas.begin(); it != aristas.end(); ++it) {
     archivo << "e " << it->first << " " << it->second << endl;
   }
 }
 
 int main(int argc, char **argv) {
+  if (argc < 5) {
+    cerr << "Uso: " << argv[0]
+         << " <nodos> <densidad (entre 0 y 1)> <particiones> <outputfile>"
+         << endl;
+  }
   int cantidadDeNodos = atoi(argv[1]);
   double densidadDeAristas = atof(argv[2]);
   int cantidadDeParticiones = atoi(argv[3]);
-  char* nombreDeArchivo = argv[4];
+  char *nombreDeArchivo = argv[4];
 
   // Genero las aristas
-  list< pair<int,int> > aristas = generarAristas(cantidadDeNodos, densidadDeAristas);
+  list<pair<int, int>> aristas =
+      generarAristas(cantidadDeNodos, densidadDeAristas);
 
   // Comienzo a escribir el archivo
   ofstream archivo(nombreDeArchivo);
   archivo << "c Instancia generada aleatoriamente" << endl;
-  archivo << "p edge " << cantidadDeNodos << " " << aristas.size() << " " << cantidadDeParticiones << endl;
+  archivo << "p edge " << cantidadDeNodos << " " << aristas.size() << " "
+          << cantidadDeParticiones << endl;
 
   // Genero las particiones
   generarParticiones(archivo, cantidadDeNodos, cantidadDeParticiones);
@@ -75,5 +100,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-
-
