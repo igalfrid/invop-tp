124506dfce1fdd17a2d64894000a8f02ec40aa53
Igal Frid <ipfrid@gmail.com>
2015-12-01 15:53:31 -0300

    Elimino la carpeta branch and bound

--- src/branch_and_bound/Particion.cpp
+++
@@ -1,7 +0,0 @@
-#include "Particion.h"
-
-void Particion::agregarNodo(int nodo) { this->nodos.push_back(nodo); }
-
-int Particion::getCantidadDeNodos() { return this->nodos.size(); }
-
-list<int> Particion::getNodos() { return this->nodos; }
