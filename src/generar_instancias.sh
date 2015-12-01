b00516c810e975cbd6b9ed4f8e82a1c1de54f073
Igal Frid <ipfrid@gmail.com>
2015-12-01 16:44:35 -0300

    El generador de instancias genera las instancias

--- src/generar_instancias.sh
+++ src/generar_instancias.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-for nodos in 30 40 50 60
+for nodos in 30 40 50
 do
 	for d in 10 25 50 75 90
 	do
-		for k in 5 10 15 20 25
+		for k in 5 10 15 20
 		do
-			./generador/generador $nodos 0.$d $k instancias/instancia-$nodos-$d-$k.col
+			for i in $(seq 1 5)
+			do
+				# Genero 10 instancias distintas
+				./generador/generador $nodos 0.$d instancias/instancia-$nodos-$d-$k.$i.col
+				
+				for j in $(seq 1 5)
+				do
+ 					# Genero 10 particiones distintas para cada instancia
+					./particionador/particionar instancias/instancia-$nodos-$d-$k.$i.col instancias/instancia-$nodos-$d-$k.$i.$j.col $k
+				done
+			done
 		done
 	done
 done
