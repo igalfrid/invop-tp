fea6cf3e1529e5c01abc54c0b5e4f675396e9ae5
Marco Vanotti <mvanotti@dc.uba.ar>
2015-12-03 14:31:52 -0300

    Ahora sol.py solo exporta a csv.
    uso: sol.py > archivo.csv

--- src/sol.py
+++ src/sol.py
@@ -32,46 +32,10 @@ def main():
               if (tt == -1):
                 continue
               soluciones[archivo] = (tt, ta)
-              sols.append(((nodos, densidad, particiones, i, j, algs), (tt, ta) )) 
-  Res = namedtuple("Res", ["nodos", "densidad", "particiones", "algs", "i", "j", "tt", "ta"])
-  ds = []
-  for (key, val) in sols:
-    k = Res(nodos=key[0], densidad=key[1], particiones=key[2], 
-            i = key[3], j = key[4], algs = key[5],
-            tt=val[0], ta = val[1])
-    ds.append(k)             
-  # en ds podemos hacer ds[0].nodos
-  promediar(ds)
+              sols.append((archivo, nodos, densidad, particiones, i, j, algs, tt, ta))
 
-def promediar(ds):
-  Clave = namedtuple("Clave", ["nodos", "densidad", "particiones", "algs", "i"])
-  dic = {}
-  for d in ds:
-    c = Clave(nodos=d.nodos, densidad=d.densidad, particiones=d.particiones, algs=d.algs, i=d.i)
-    if c not in dic:
-      dic[c] = []
-    dic[c].append((d.tt, d.ta))
-  
-  for k in dic:
-    dic[k] = (avg([t[0] for t in dic[k]]), avg([t[1] for t in dic[k]]))
-
-  # Ahora calculamos los promedios de estos promedios (agrupamos por i)
-  Clave = namedtuple("Clave", ["nodos", "densidad", "particiones", "algs"])
-  dic2 = {}
-  for k in dic:
-    c = Clave(nodos=k.nodos, densidad=k.densidad, particiones=k.particiones, algs=k.algs)
-    if c not in dic2:
-      dic2[c] = []
-    dic2[c].append(dic[k])
-
-  for k in dic2:
-    dic2[k] = (avg([t[0] for t in dic2[k]]), avg([t[1] for t in dic2[k]]))
-  
-  for k in sorted(dic2.keys()):
-    print k, dic2[k]
-  
-def avg(ls):
-  return sum(ls)/float(len(ls))
-  
+  print "archivo,nodos,densidad,particiones,i,j,algs,tt,ta"
+  for sol in sorted(sols):
+    print ",".join(map(str, sol)) 
 if __name__ == "__main__":
   main()
